2abf9f2d4367a87d7e4127ecf57755d35c9be3fb
micahlanham <micahlanham@gmail.com>
2021-10-29 13:39:19 -0400

    added reference and object store

--- public/js/idb.js
+++ public/js/idb.js
@@ -3,3 +3,13 @@ let db;
 
 // establish connection to IndexedDB called 'budget-tracker' and set it to version 1
 const request = indexedDB.open('budget_tracker', 1);
+
+// this event will emit if the database version changes
+request.onupgradeneed = function(event) {
+
+    // save a reference to db
+    const db = event.target.result;
+
+    // create an object store called 'new_transaction' and set it for auto incrementing primary key
+    db.createObjectStore('new_transaction', { autoIncrement: true});
+};
