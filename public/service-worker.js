f4e9779d3ae51e33a758c9f047fe596443feaec3
micahlanham <micahlanham@gmail.com>
2021-10-29 14:56:30 -0400

    updated with event listener

--- public/service-worker.js
+++ public/service-worker.js
@@ -17,3 +17,28 @@ const FILE_TO_CACHE = [
     './icons/icon-96x96.png',
     './icons/icon-72x72.png'
 ];
+
+self.addEventListener('install', function (e) {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then(function (cache) {
+            console.log('installing cache :' + CACHE_NAME);
+        })
+    )
+});
+
+self.addEventListener('activate', function (e) {
+    e.waitUntil(
+        caches.keys().then(function (keyList) {
+            let cacheKeeplist = keyList.filter(function (key) {
+                return key.indexOf(APP_PREFIX);
+            });
+            cacheKeeplist.push(CACHE_NAME);
+
+            return Promise.all(keyList.map(function (key, i) {
+                if (cacheKeeplist.indexOf(key) === -1) {
+                    console.log('deleting cache :' + keyList[i] );
+                }
+            }));
+        })
+    )
+});
