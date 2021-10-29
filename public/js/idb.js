3f3222a4ca1663e41885d6cc872ec7a0841ddb8c
micahlanham <micahlanham@gmail.com>
2021-10-29 13:42:38 -0400

    added to code

--- public/js/idb.js
+++ public/js/idb.js
@@ -13,3 +13,15 @@ request.onupgradeneed = function(event) {
     // create an object store called 'new_transaction' and set it for auto incrementing primary key
     db.createObjectStore('new_transaction', { autoIncrement: true});
 };
+
+// when successful
+request.onsuccess = function(event) {
+
+    // when db is created succesfully with its object store
+    db = event.target.result;
+
+    // check if app is online. if yes then run uploadTransaction() function to send local db data to api
+    if (navigator.online) {
+
+    }
+};
